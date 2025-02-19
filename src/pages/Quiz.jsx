743df791e7648a0b5703d3e3217f86008f8e7902
Bahodirjon <Bahodirjon@gmail.com>
2025-02-19 16:05:58 +0200

    Frontend Quizz

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -15,7 +15,7 @@ function Quiz() {
     isPending,
     error,
   } = useFetch(
-    `https://online-json-server-api.up.railway.app/project/66589e8316aab5687eae3b28/quizzes?title=${title}`
+    ` https://json-api.uz/api/project/f-quiz/quizzes?title=${title}`
   );
 
   useEffect(() => {
